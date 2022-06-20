80d0d96a27ab853a209b6662b0fb1eee0eeeae47
Bricius13 <charifibrice.pro@gmail.com>
2022-06-20 10:54:32 +0200

    Quete React int02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Quotes from './components/Quotes';
+import axios from 'axios';
+import React from 'react';
+import { useEffect, useState } from 'react';
+
 function App() {
+
+  const [simpsons, setSimpsons] = useState(' ');
+  
+  const getQuotes = () => {
+    axios
+    .get('https://simpsons-quotes-api.herokuapp.com/quotes?count=10')
+    .then((response) => response.data)
+    .then((data) => {
+      setSimpsons(data);
+    })
+  }
+  
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button className='quoteButton' onClick={getQuotes}>Cick for simpsons quote</button>
+      <Quotes quote={simpsons[0].quote} character={simpsons[0].character} image={simpsons[0].image} />
     </div>
   );
 }
---
+++ src/components/Quotes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Quotes.css';
+
+
+
+const Quotes = ({ quote, character, image }) => (
+  <figure className="QuoteCard">
+    <img src={image} alt={character} />
+    <figcaption>
+      <blockquote>{quote}</blockquote>
+      <cite>{character}</cite>
+    </figcaption>
+  </figure>
+);
+
+export default Quotes;
